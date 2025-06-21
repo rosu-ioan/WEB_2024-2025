885078212d5170f8b310f4da93df3408eeab7dd9
Rosu Ioan <rosu.ioan@proton.me>
2025-06-21 23:06:31 +0300

    update autoload

--- utils/autoload.php
+++ utils/autoload.php
@@ -1,11 +1,18 @@
 <?php
-    define('ROOT', __DIR__);
+    define('ROOT', dirname(__DIR__));
     define('SLASH', DIRECTORY_SEPARATOR);
 
-    $folders = ['controllers', 'models', 'views', 'utils'];
+    $folders = ['controllers', 'models', 'views', 'utils', 'templates'];
 
     function autoload($class) {
         global $folders;
+
+        $vendorAutoloadPath = ROOT . SLASH . 'vendor' . SLASH . 'autoload.php';
+        if(file_exists($vendorAutoloadPath)) {
+            require_once $vendorAutoloadPath;
+        } else {
+            throw new Exception("Vendor autoload cannot be found");
+        }
 
         $path = ROOT . SLASH . '<folder>' . SLASH . lcfirst($class) . '.php';
         foreach ($folders as $folder) {
